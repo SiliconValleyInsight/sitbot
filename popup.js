50bd12dcc7382f5a2abf3f5ae0d03cf422adb8a9
benigeri <me@benigeri.com>
2015-05-04 21:57:53 -0700

    working logic

--- popup.js
+++ popup.js
@@ -1,4 +1,6 @@
 SITBOT_EXTENSION = (function() {
+  var profiles = {}
+  var progress;
 
   function changeUrl(url) {
     chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
@@ -40,29 +42,97 @@ SITBOT_EXTENSION = (function() {
   }
 
   function startScraping(){
-    chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
-      var port = chrome.tabs.connect(tab[0].id, {name: "scraper"});
-      port.postMessage({type: "start_scraping"});
-      port.onMessage.addListener(function(msg) {
-        if (msg.type == "scraped_profiles") {
-          saveProfiles(msg);
+    //chrome.runtime.sendMessage({type: "start_scraping"});
+    chrome.tabs.executeScript(null, {file: "jquery.min.js"});
+    chrome.tabs.executeScript(null, {file: "sitbot.js"});
+
+    chrome.runtime.onMessage.addListener(
+      function(request, sender, sendResponse) {
+        if (request.type == 'scraped_profiles') {
+          saveProfileBatch(request);
         }
+        return;
       });
-    });
   }
 
-  function saveProfiles(data) {
+  function saveProfileBatch(data) {
       //alert("I received the following DOM content:\n" + element);
-      console.log('and here we are');
-      console.log(data);
+      profiles[data.current_page] = data.profiles;
+      updateProgressIndicator(data);
+      if (data.current_page == data.total_pages) {
+        processAndShowResults();
+        return;
+      }
+      changeUrl(data.next_page_URL);
+      window.setTimeout(injectContentScripts,3000);
+  }
+
+  function initProgressIndicator(total_pages) {
+    progress = $('.progress');
+    progress.removeClass('hidden');
+    progress.find('.progress__total_pages').text(total_pages);
+  }
+
+  function updateProgressIndicator(data) {
+    if(typeof(progress) == "undefined") {
+      initProgressIndicator(data.total_pages);
+    }
+    progress.find('.progress__current_page').text(data.current_page);
+  }
+
+  function generateDownloadLink(data) {
+    var MIME_TYPE = 'text/plain';
+
+    window.URL = window.webkitURL || window.URL;
+
+    var data_blob = new Blob([data], {type: MIME_TYPE});
+
+    var a = document.createElement('a');
+
+    a.download = 'sitbot_results.csv';
+    a.href = window.URL.createObjectURL(data_blob);
+    a.textContent = 'Download ready';
+
+    a.dataset.downloadurl = [MIME_TYPE, a.download, a.href].join(':');
+    a.draggable = true; // Don't really need, but good practice.
+
+    $('.container').append(a);
+  }
+
+  function processAndShowResults() {
+    var data = "";
+    for (page_no in profiles) {
+      profiles[page_no].map(function(profile) {
+        data += JSONToCSV(profile);
+      });
+    }
+    generateDownloadLink(data);
+  }
+
+  function JSONToCSV(object) {
+    var keys = ['url', 'name', 'email']
+    var output = "";
+    var i;
+
+    for (i = 0; i < keys.length; i++) {
+      var key = keys[i];
+      output += '\"' + object[key] + '\"' +','
+    }
+
+    output += '\n';
+    return output;
+  }
+
+
+  function injectContentScripts() {
+      chrome.tabs.executeScript(null, {file: "jquery.min.js"});
+      chrome.tabs.executeScript(null, {file: "sitbot.js"});
   }
 
   return {
     init: function() {
       $('#github-form').submit(githubSearch);
       $('#scrape-button').click(startScraping);
-      chrome.tabs.executeScript(null, {file: "jquery.min.js"});
-      chrome.tabs.executeScript(null, {file: "sitbot.js"});
     }
   }
 });
--- sitbot.js
+++ sitbot.js
@@ -4,7 +4,7 @@ SITBOT_CRAWLER = (function () {
   var new_results_arrived = false;
 
   function getTotalPages() {
-    var pagination_links = $('.pagination a');
+    var pagination_links = $('.pagination').children();
     var page_numbers = pagination_links.map(function() {
       var val = parseInt($(this).text());
       if (!isNaN(val)) {
@@ -14,27 +14,35 @@ SITBOT_CRAWLER = (function () {
     return Math.max.apply(null, page_numbers);
   }
 
-  function startScraping(port) {
-    total_pages = getTotalPages();
-    scrapeAndSendProfiles(port);
-    clickNextPage();
-    listenToPageUpdate(port);
+  function getNextPageURL() {
+    var prefix = "https://github.com";
+    var suffix = $('.next_page').attr('href');
+    return prefix + suffix;
   }
 
-  function clickNextPage() {
-    window.location.href = "http://google.com"
-    $('.next_page').click();
-    new_results_arrived = false;
+  function getCurrentPage() {
+    return parseInt($('.pagination .current').text());
   }
 
-  function listenToPageUpdate(port) {
-    $('#site-container').on('DOMNodeInserted', function(e) {
-      if (!new_results_arrived) {
-        new_results_arrived = true;
-        //scrapeAndSendProfiles(port);
-        clickNextPage();
-      }
+  function scrapePage(port) {
+    var total_pages = getTotalPages();
+    var cur_page = getCurrentPage();
+    var next_page_URL = getNextPageURL();
+    var profiles = scrapeProfiles(port);
+
+    sendMessage({
+      type: "scraped_profiles",
+      next_page_URL: next_page_URL,
+      current_page: cur_page,
+      total_pages: total_pages,
+      profiles: profiles
     });
+  }
+
+  function sendMessage(msg) {
+    console.log('SENDING MSG!');
+    console.log(msg);
+    chrome.runtime.sendMessage(msg);
   }
 
   function chromeRuntimeMessageListener(port) {
@@ -42,13 +50,13 @@ SITBOT_CRAWLER = (function () {
       scrape_port = port;
       port.onMessage.addListener(function(msg) {
         if (msg.type == "start_scraping") {
-          startScraping(port);
+          scrapePage(port);
         }
       });
     }
   }
 
-  function scrapeAndSendProfiles(port) {
+  function scrapeProfiles(port) {
     var profiles = $('.user-list-item');
     var profs = []
 
@@ -62,21 +70,30 @@ SITBOT_CRAWLER = (function () {
       p.url = 'http://github.com/' + p.username
       profs.push(p);
     })
-
-//    port.postMessage({
- //     type: "scraped_profiles",
-  //    current_page: cur_page,
-   //   total_pages: total_pages,
-    //  profiles: profs
-   // });
+    return profs;
   }
 
   return {
     init: function() {
-      chrome.runtime.onConnect.addListener(chromeRuntimeMessageListener);
+      scrapePage();
+     /*chrome.runtime.sendMessage({type:"testmsg"});
+      scrapePage();
+      chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse) {
+          if (request.type == "start_scraping") {
+            scrapePage();
+          } else {
+            console.log(request);
+          }
+      }); */
     }
   }
 });
 
 SitbotCrawler = SITBOT_CRAWLER();
-SitbotCrawler.init();
+
+$(function() {
+  SitbotCrawler.init();
+});
+
+console.log("yo dis script chillz");
